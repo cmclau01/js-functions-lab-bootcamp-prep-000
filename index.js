bd2900d75213aa343083e5427cced8be9ae42c31
Christopher McLaughlin <cmclau01@gmail.com>
2018-06-12 15:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+// 1
+function happyHolidays () {
+  return "Happy holidays!";
+}
+
+// 2
+function happyHolidaysTo(holiday, name) {
+  return `Happy holidays, ${name}!`;
+}
